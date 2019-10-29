10cb54821cba7c9dceabc701a67060573164fcca
Michael Hart <michael.hart.au@gmail.com>
2019-10-29 17:30:56 -0400

    Add install scripts for examples

---
+++ examples/nodejs10.x/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf dependencies
+mkdir -p dependencies
+
+docker run --rm -v "$PWD"/dependencies:/lambda/opt lambci/yumda:2 yum install -y git GraphicsMagick jp2a
+
+# To test:
+
+# sam local invoke --no-event
+
+# OR
+
+# sls invoke local --docker -f hello-world
